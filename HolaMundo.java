1b20ea3cf977757cf2393de5711af274e16e32ed
atn25042002 <aacot@unsa.edu.pe>
2022-04-29 11:57:52 -0500

    Personalizando el archivo HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,9 +1,9 @@
-
 public class HolaMundo {
 	public static void main (String args[]){
 		/** */
 		//HolaMundo/
 		System.out.println("¡Hola mundo!");
+		System.out.println("Soy Anthony Edwin Aco Tito");
 
 	}
 }
